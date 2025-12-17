81ec0b5369474219723fa04ae5f7f39710921bf5
choerubi <iida.porola@helsinki.fi>
2025-12-17 22:25:39 +0200

    Add wall tiles

--- src/game_loop.py
+++ src/game_loop.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import random
 import pygame
 import rooms
 import bowyer_watson
@@ -10,7 +11,7 @@ from config import (
     DUNGEON_WIDTH, DUNGEON_HEIGHT, TILE_SIZE
 )
 
-# pylint: disable=too-many-instance-attributes,too-many-nested-blocks
+# pylint: disable=too-many-instance-attributes,too-many-nested-blocks,too-many-statements
 class GameLoop:
     def __init__(self):
         """A constructor that initializes the game window."""
@@ -37,6 +38,7 @@ class GameLoop:
         self.mst_edges = None
         self.extra_edges = None
         self.tiles = None
+        self.floor_map = None
 
         self.bowyer_watson = bowyer_watson.BowyerWatson()
         self.prim = prim.Prim()
@@ -94,11 +96,39 @@ class GameLoop:
     def _load_assets(self):
         """A method that loads the tile assets."""
 
-        self.floor_tile = pygame.image.load(
-            os.path.join(self.dir_name, "assets", "sprite_022.png")
+        self.floor_tiles = [
+            pygame.image.load(
+                os.path.join(self.dir_name, "assets", "sprite_01.png")
+            ).convert(),
+            pygame.image.load(
+                os.path.join(self.dir_name, "assets", "sprite_02.png")
+            ).convert()
+        ]
+
+        self.top_wall_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_03.png")
         ).convert()
-        self.path_tile = pygame.image.load(
-            os.path.join(self.dir_name, "assets", "sprite_023.png")
+        self.bottom_wall_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_04.png")
+        ).convert()
+        self.left_wall_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_05.png")
+        ).convert()
+        self.right_wall_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_06.png")
+        ).convert()
+
+        self.top_l_corner_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_07.png")
+        ).convert()
+        self.top_r_corner_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_08.png")
+        ).convert()
+        self.bottom_l_corner_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_09.png")
+        ).convert()
+        self.bottom_r_corner_tile = pygame.image.load(
+            os.path.join(self.dir_name, "assets", "sprite_10.png")
         ).convert()
 
     def start(self):
@@ -145,14 +175,13 @@ class GameLoop:
         self.rooms = rooms.generate_rooms(
             grid_width=DUNGEON_WIDTH // TILE_SIZE,
             grid_height=DUNGEON_HEIGHT // TILE_SIZE,
-            min_size=2,
+            min_size=4,
             max_size=10,
             max_rooms=12,
             margin=2
         )
 
         room_centers = []
-
         for room in self.rooms:
             center_x = room.tile_x * TILE_SIZE + room.tile_width * TILE_SIZE // 2
             center_y = room.tile_y * TILE_SIZE + room.tile_height * TILE_SIZE // 2
@@ -165,7 +194,28 @@ class GameLoop:
 
         self.extra_edges = prim.add_random_edges(15, self.mst_edges, self.triangles, self.rooms)
 
-        self._get_free_tiles()
+        self._map_tiles()
+
+    def _map_tiles(self):
+        """A method that marks non-room tiles as zeros and room tiles as ones.
+
+        The method also initializes a floor map that is used for drawing floor tiles."""
+
+        # Initialize all tiles as zeros
+        tile_height = DUNGEON_HEIGHT // TILE_SIZE
+        tile_width = DUNGEON_WIDTH // TILE_SIZE
+        self.tiles = [[0 for col in range(tile_height)] for row in range(tile_width)]
+
+        self.floor_map = [[None for col in range(tile_height)] for row in range(tile_width)]
+
+        # Mark tiles inside rooms as ones
+        for room in self.rooms:
+            for i in range(room.tile_width):
+                for j in range(room.tile_height):
+                    tile_x = room.tile_x + i
+                    tile_y = room.tile_y + j
+                    self.tiles[tile_x][tile_y] = 1
+                    self.floor_map[tile_x][tile_y] = random.choice(self.floor_tiles)
 
     def _draw_rooms(self):
         """A method that draws the rooms onto the dungeon surface."""
@@ -183,7 +233,63 @@ class GameLoop:
                         TILE_SIZE
                     )
 
-                    self.dungeon_surface.blit(self.floor_tile, tile_rect)
+                    floor_tile = self.floor_map[tile_x][tile_y]
+                    self.dungeon_surface.blit(floor_tile, tile_rect)
+
+    def _get_tile_position(self, tile_x, tile_y):
+        """A method that finds the position of a tile in a room based on the surrounding tiles."""
+
+        if 0 <= tile_x < len(self.tiles) - 1:
+            if 0 <= tile_y < len(self.tiles[0]) - 1:
+
+                up = self.tiles[tile_x][tile_y - 1] == 1
+                down = self.tiles[tile_x][tile_y + 1] == 1
+                left = self.tiles[tile_x - 1][tile_y] == 1
+                right = self.tiles[tile_x + 1][tile_y] == 1
+
+                return (up, down, left, right)
+
+        return False
+
+    def _draw_wall_tiles(self):
+        """A method that draws the wall tiles onto the dungeon surface."""
+
+        for tile_x in range(len(self.tiles)):
+            for tile_y in range(len(self.tiles[tile_x])):
+
+                if self.tiles[tile_x][tile_y] == 1:
+                    tile_pos = self._get_tile_position(tile_x, tile_y)
+                    tile = None
+
+                    if tile_pos == (True, True, True, True):
+                        continue
+
+                    if tile_pos == (False, True, True, True):
+                        tile = self.top_wall_tile
+                    elif tile_pos == (True, False, True, True):
+                        tile = self.bottom_wall_tile
+                    elif tile_pos == (True, True, False, True):
+                        tile = self.left_wall_tile
+                    elif tile_pos == (True, True, True, False):
+                        tile = self.right_wall_tile
+
+                    elif tile_pos == (False, True, False, True):
+                        tile = self.top_l_corner_tile
+                    elif tile_pos == (False, True, True, False):
+                        tile = self.top_r_corner_tile
+                    elif tile_pos == (True, False, False, True):
+                        tile = self.bottom_l_corner_tile
+                    elif tile_pos == (True, False, True, False):
+                        tile = self.bottom_r_corner_tile
+
+                    tile_rect = pygame.Rect(
+                                tile_x * TILE_SIZE,
+                                tile_y * TILE_SIZE,
+                                TILE_SIZE,
+                                TILE_SIZE
+                    )
+
+                    self.dungeon_surface.blit(tile, tile_rect)
 
     def _draw_triangulation(self):
         """A method that draws the Delaunay triangulation onto the dungeon surface."""
@@ -205,48 +311,43 @@ class GameLoop:
         for edge in self.extra_edges:
             pygame.draw.line(self.dungeon_surface, (57, 255, 20), edge.v1, edge.v2)
 
-    def _get_free_tiles(self):
-        """A method that finds all free tiles that are not blocked by rooms."""
-
-        # Initialize all tiles as zeros (free tiles)
-        tile_height = DUNGEON_HEIGHT // TILE_SIZE
-        tile_width = DUNGEON_WIDTH // TILE_SIZE
-        self.tiles = [[0 for col in range(tile_height)] for row in range(tile_width)]
-
-        # Mark tiles inside rooms as ones (occupied tiles)
-        for room in self.rooms:
-            for i in range(room.tile_width):
-                for j in range(room.tile_height):
-                    tile_x = room.tile_x + i
-                    tile_y = room.tile_y + j
-                    self.tiles[tile_x][tile_y] = 1
-
     def _draw_paths(self):
         """A method that draws the A* paths onto the dungeon surface."""
 
         for edge in self.mst_edges.union(self.extra_edges):
-            tile_x1 = edge.v1[0] // TILE_SIZE
-            tile_y1 = edge.v1[1] // TILE_SIZE
-            start_pos = tile_x1, tile_y1
-
-            tile_x2 = edge.v2[0] // TILE_SIZE
-            tile_y2 = edge.v2[1] // TILE_SIZE
-            goal_pos = tile_x2, tile_y2
+            start_pos = (edge.v1[0] // TILE_SIZE, edge.v1[1] // TILE_SIZE)
+            goal_pos = (edge.v2[0] // TILE_SIZE, edge.v2[1] // TILE_SIZE)
 
             path = self.a_star.find_path(start_pos, goal_pos, self.tiles)
 
             for tile_x, tile_y in path:
-                tile_rect = pygame.Rect(
+                if self.tiles[tile_x][tile_y] == 0:
+                    tile_rect = pygame.Rect(
                         tile_x * TILE_SIZE,
                         tile_y * TILE_SIZE,
                         TILE_SIZE,
                         TILE_SIZE
                     )
+                    floor_tile = self.floor_tiles[(tile_x + tile_y) % 2]
+                    self.dungeon_surface.blit(floor_tile, tile_rect)
 
-                if self.tiles[tile_x][tile_y] == 0:
-                    self.dungeon_surface.blit(self.path_tile, tile_rect)
+                    neighbor_tiles = [
+                        (tile_x, tile_y - 1),
+                        (tile_x, tile_y + 1),
+                        (tile_x - 1, tile_y),
+                        (tile_x + 1, tile_y)
+                    ]
 
-# pylint: disable=too-many-statements
+                    for new_x, new_y in neighbor_tiles:
+                        if self.tiles[new_x][new_y] == 1:
+                            tile_rect = pygame.Rect(
+                                new_x * TILE_SIZE,
+                                new_y * TILE_SIZE,
+                                TILE_SIZE,
+                                TILE_SIZE
+                            )
+                            self.dungeon_surface.blit(floor_tile, tile_rect)
+
     def _render(self):
         """A method that renders the current game frame."""
 
@@ -256,6 +357,7 @@ class GameLoop:
         if self.rooms:
             if self.current_view >= 0:
                 self._draw_rooms()
+                self._draw_wall_tiles()
 
             if self.current_view == 1:
                 self._draw_triangulation()
